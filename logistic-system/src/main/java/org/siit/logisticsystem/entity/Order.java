238eb09f3c925c1e6d1e3191c0511ed78d6b558d
vitalie-stasii <vitalie.stasii@gmail.com>
2023-03-05 20:46:20 +0200

    LS-09 Populate tables from CSV files at application startup.

--- logistic-system/src/main/java/org/siit/logisticsystem/entity/Order.java
+++ logistic-system/src/main/java/org/siit/logisticsystem/entity/Order.java
@@ -3,32 +3,32 @@ package org.siit.logisticsystem.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import org.siit.logisticsystem.enums.OrderStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
-@Builder
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private final long id;
+    private long id;
     @NotNull
     @ManyToOne
     @JoinColumn(name = "destination_id", nullable = false)
-    private final Destination destinationID;
+    private Destination destinationID;
     @NotNull
     @JsonFormat(pattern = "dd-MM-yyyy")
     @Column(name = "delivery_date")
-    private final LocalDate deliveryDate;
+    private LocalDate deliveryDate;
     @NotNull
     @Column(name = "status")
-    private final OrderStatus status;
+    private OrderStatus status;
     @NotNull
     @Column(name = "last_updated")
-    private final LocalDateTime lastUpdated;
+    private LocalDateTime lastUpdated;
 }
