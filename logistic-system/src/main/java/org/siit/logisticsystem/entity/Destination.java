cc61c3d91f4526f5f049eda87d6e6e3b6ebbac47
tomac <toma_costel2002@yahoo.com>
2023-03-07 19:56:09 +0200

    E02 e1

--- logistic-system/src/main/java/org/siit/logisticsystem/entity/Destination.java
+++ logistic-system/src/main/java/org/siit/logisticsystem/entity/Destination.java
@@ -3,22 +3,21 @@ package org.siit.logisticsystem.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.Builder;
+import lombok.Data;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@Data
+@Builder
 @Entity(name = "destinations")
 public class Destination {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private final long id;
     @NotNull
     @Column(name = "name", unique = true)
-    private String name;
+    private final String name;
     @NotNull
     @Column(name = "distance")
     @Min(value = 0)
-    private double distance;
+    private final double distance;
 }
--- logistic-system/src/main/java/org/siit/logisticsystem/entity/Order.java
+++ logistic-system/src/main/java/org/siit/logisticsystem/entity/Order.java
@@ -3,32 +3,32 @@ package org.siit.logisticsystem.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.Builder;
+import lombok.Data;
 import org.siit.logisticsystem.enums.OrderStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
+@Builder
 @Entity(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private final long id;
     @NotNull
     @ManyToOne
     @JoinColumn(name = "destination_id", nullable = false)
-    private Destination destinationID;
+    private final Destination destinationID;
     @NotNull
     @JsonFormat(pattern = "dd-MM-yyyy")
     @Column(name = "delivery_date")
-    private LocalDate deliveryDate;
+    private final LocalDate deliveryDate;
     @NotNull
     @Column(name = "status")
-    private OrderStatus status;
+    private final OrderStatus status;
     @NotNull
     @Column(name = "last_updated")
-    private LocalDateTime lastUpdated;
+    private final LocalDateTime lastUpdated;
 }
