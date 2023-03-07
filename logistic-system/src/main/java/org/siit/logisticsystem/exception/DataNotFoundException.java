ea23c6b39dbfeb6e058093635b913d84ee945dcf
lucadani7 <lucaionescu1998@gmail.com>
2023-03-07 18:40:38 +0200

    Created OrderController class

---
+++ logistic-system/src/main/java/org/siit/logisticsystem/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package org.siit.logisticsystem.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
