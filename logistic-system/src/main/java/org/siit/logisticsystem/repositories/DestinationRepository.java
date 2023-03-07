16b613c32d4868041b758cfaabd3210140c19d37
tomac <toma_costel2002@yahoo.com>
2023-03-07 20:44:10 +0200

    E02 e14234

--- logistic-system/src/main/java/org/siit/logisticsystem/repositories/DestinationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.siit.logisticsystem.repositories;
-
-import org.siit.logisticsystem.entity.Destination;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinationRepository extends JpaRepository<Destination,Long> {
-
-
-}
