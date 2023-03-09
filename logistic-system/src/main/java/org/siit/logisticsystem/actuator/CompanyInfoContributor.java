f5e3ee25713fa8a33d9c5cc311a38977f87cf7b3
Razvan Radulescu <48254745+razvan-radulescu@users.noreply.github.com>
2023-03-09 19:44:04 +0200

    LS08
    Actuatorconfig
    CustomInfoEndpoint
    
    modificari cu currentDate - CurrentData

--- logistic-system/src/main/java/org/siit/logisticsystem/actuator/CompanyInfoContributor.java
+++ logistic-system/src/main/java/org/siit/logisticsystem/actuator/CompanyInfoContributor.java
@@ -1,5 +1,6 @@
 package org.siit.logisticsystem.actuator;
 
+import org.siit.logisticsystem.controller.CurrentData;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
@@ -8,15 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CompanyInfoContributor implements InfoContributor {
 
-    private final OrderService orderService;
+    private final CurrentData currentData;
 
-    public CompanyInfoContributor(OrderService orderService) {
-        this.orderService = orderService;
+    public CompanyInfoContributor(CurrentData currentData) {
+        this.currentData = currentData;
     }
 
     @Override
     public void contribute(Info.Builder builder) {
-        builder.withDetail("currentDate", orderService.getCurrentDate())
-                .withDetail("profit", orderService.calculateProfit());
+        builder.withDetail("currentDate", currentData.toString());
+               // .withDetail("profit", orderService.calculateProfit());
     }
 }
