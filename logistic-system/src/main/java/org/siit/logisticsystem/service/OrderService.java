530684c79386e8a05c8bff6d8c804a673f089c80
vitalie-stasii <vitalie.stasii@gmail.com>
2023-03-09 19:37:14 +0200

    Created OrderService, updated CompanyInfoContributor

---
+++ logistic-system/src/main/java/org/siit/logisticsystem/service/OrderService.java
@@ -0,0 +1,12 @@
+package org.siit.logisticsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    public void calculateProfit() {
+
+    }
+
+}
